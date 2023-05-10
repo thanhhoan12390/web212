b88bb47ba51b8a72136068894a7cc7a064d23f4d
Hoan Phan <104263239+thanhhoan12390@users.noreply.github.com>
2023-05-10 19:05:39 +0700

    Add files via upload

---
+++ mvc/views/admin/categories/edit.php
@@ -0,0 +1,8 @@
+<h1>Editing Category</h1>
+
+
+
+<?php 
+    require_once "../web212/mvc/views/admin/categories/form_category.php";
+?>
+<a href="http://localhost/web212/Category/index">Back</a>
---
+++ mvc/views/admin/categories/index.php
@@ -0,0 +1,56 @@
+<?php
+if (isset($_SESSION["email"]) && $_SESSION["role"] == "customer") {
+  echo '<script type = "text/javascript">
+  window.location.href = "http://localhost/web212/"</script>';
+}
+?>
+
+<div class="page-header">
+  <div class="row align-items-center mb-3">
+    <div class="col-sm mb-2 mb-sm-0">
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb breadcrumb-no-gutter">
+          <li class="breadcrumb-item"><a class="breadcrumb-link" href="http://localhost/web212/category/index"></a></li>
+          <!-- icon home -->
+          <li class="breadcrumb-item active" aria-current="page">Categories</li>
+        </ol>
+      </nav>
+
+      <h1 class="page-header-title">Categories<span class="badge badge-soft-dark ml-2"><?php echo mysqli_num_rows($data["categories"])?></span></h1>
+    </div>
+    <div class="col-sm-auto">
+      <a href="http://localhost/web212/Category/new" class="btn btn-primary">Add Category</a>
+    </div>
+  </div>
+</div>
+
+<div class="card">
+  <div class="table-responsive">
+    <table class="table table-borderless card-table">
+      <thead class="thead-light">
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th colspan="2"></th>
+      </tr>
+      </thead>
+
+      <tbody>
+      <?php 
+        while($row = mysqli_fetch_array($data["categories"])){
+      ?>
+        <tr>
+          <td><?php echo $row["category_id"] ?></td>
+          <td><?php echo $row["category_name"] ?></td>
+          <td><a href="http://localhost/web212/Category/edit/<?php echo $row["category_id"] ?>">Edit</td>
+          <td><a href="http://localhost/web212/Category/delete/<?php echo $row["category_id"] ?>">Delete</td>
+        </tr>
+      <?php 
+        }
+      ?>
+      
+      </tbody>
+    </table>
+  </div>
+</div>
+
